b673e6e33b73530be6441661db7b136856908ca8
Alexandr <sanchello2001@yandex.ru>
2020-03-30 15:18:08 +0300

    начал делать список избранных

--- app/src/main/java/com/example/studmy/LikeFragment.java
+++ app/src/main/java/com/example/studmy/LikeFragment.java
@@ -4,6 +4,7 @@ package com.example.studmy;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class LikeFragment extends Fragment {
 
+    RecyclerView recyclerView;
+
 
     public LikeFragment() {
         // Required empty public constructor
@@ -24,8 +27,11 @@ public class LikeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_like, container, false);
+        View view = inflater.inflate(R.layout.fragment_like, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewLike);
+
+        return view ;
     }
 
 }
